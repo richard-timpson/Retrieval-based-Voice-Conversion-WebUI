910b4b5cb02ff0dab98928b14edd13e318474f9b
Richard Timpson <richardtimpson80@gmail.com>
2025-05-26 04:02:42 -0600

    Make inference work with a 5090

--- infer/modules/vc/utils.py
+++ infer/modules/vc/utils.py
@@ -1,6 +1,7 @@
 import os
-
+import torch
 from fairseq import checkpoint_utils
+from fairseq.data.dictionary import Dictionary
 
 
 def get_index_path_from_model(sid):
@@ -20,6 +21,9 @@ def get_index_path_from_model(sid):
 
 
 def load_hubert(config):
+    # this is a hack to make sure the torch.load call in fairseq works correctly
+    # this code should really exist in fairseq, i guess it doesn't suppor PyTorch after 2.6
+    torch.serialization.add_safe_globals([Dictionary])
     models, _, _ = checkpoint_utils.load_model_ensemble_and_task(
         ["assets/hubert/hubert_base.pt"],
         suffix="",
